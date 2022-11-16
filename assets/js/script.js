946c32b70cfab4b6586e1dadfc804cf27bc73431
naragurgel <gurgel.nara@gmail.com>
2022-11-16 21:09:41 +0000

    set up data style input

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,37 @@ let main = document.getElementById('listReso');
 
 function addResolution() {
     //get the input data
-    let valueInput = input.value;
+    let dataInput = input.value;
 
     //If it is not empty, null or undefined 
-    if((valueInput !=="") && (valueInput !== null) && (valueInput !== undefined)) {
+    if((dataInput !=="") && (dataInput !== null) && (dataInput !== undefined)) {
         let newItem= `<div class="item">
         <div class="icon-item">
             <i class="mdi mdi-circle-outline"></i>
 
         </div>
         <div class="icon-name">
-            Teste
+            ${dataInput}
         </div>
         <div class="icon-button">
             <button class="delete"><i class="mdi mdi-delete"></i>Delete</button>
             
         </div>
-    </div>`;
+        </div>`;
 
-    main.innerHTML += newItem;
+        //add new item to the main section
+        main.innerHTML += newItem;
+
+        //clean the inputs 
+        input.value= "";
+        input.focus();
 
     }
 }
+
+input.addEventListener("keyup", function(event) {
+    if(event.keyCode === 'Enter') {
+        event.preventDefault();
+        btnAdd.click();
+    }
+})
