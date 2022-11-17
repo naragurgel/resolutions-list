22cd6bee31da7588e8219e54dc300b985fe21b31
naragurgel <gurgel.nara@gmail.com>
2022-11-17 20:47:54 +0000

    add focus and enter tab functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+let count = 0;
 let input = document.getElementById('inputReso');
 let btnAdd = document.getElementById('btn-add');
 let main = document.getElementById('listReso');
@@ -8,16 +9,19 @@ function addResolution() {
 
     //If it is not empty, null or undefined 
     if((dataInput !=="") && (dataInput !== null) && (dataInput !== undefined)) {
-        let newItem= `<div class="item">
-        <div class="icon-item">
+        
+        ++count;
+        
+        let newItem= `<div id="${count}" class="item">
+        <div onclick="tagTask(${count})" class="icon-item">
             <i class="mdi mdi-circle-outline"></i>
 
         </div>
-        <div class="icon-name">
+        <div onclick="tagTask(${count})" class="icon-name">
             ${dataInput}
         </div>
         <div class="icon-button">
-            <button class="delete"><i class="mdi mdi-delete"></i>Delete</button>
+            <button onclick="delet(${count})" class="delet"><i class="mdi mdi-delete"></i>Delete</button>
             
         </div>
         </div>`;
@@ -32,9 +36,17 @@ function addResolution() {
     }
 }
 
+function delet(id) {
+    var task = document.getElementById(id);
+    task.remove();
+}
+
+
+//when enter is pressed, submit the item to the list
 input.addEventListener("keyup", function(event) {
-    if(event.keyCode === 'Enter') {
+    if(event.keyCode === 13) {
         event.preventDefault();
         btnAdd.click();
     }
 })
+
